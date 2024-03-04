d9ada685adfa445d4926afe300fa36258ff7e0b0
Matheus <matheusmenezesribeiro@hotmail.com>
2024-03-04 18:55:08 -0300

    Add tag view model

--- TechChallenger/src/Core/Application/UseCases/TagUseCase.cs
+++ TechChallenger/src/Core/Application/UseCases/TagUseCase.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Application.ViewModel;
+using Domain.Entities;
 using Domain.Repositories;
 
 namespace Application.UseCases;
---
+++ TechChallenger/src/Core/Application/ViewModel/TagViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModel
+{
+    internal class TagViewModel
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string ImageUrl { get; private set; }
+
+        public static TagViewModel ToResult(Tag tag)
+            => new()
+            {
+                Id = tag.Id,
+                Name = tag.Name,
+                ImageUrl = tag.ImageUrl,
+            };
+    }
+}
